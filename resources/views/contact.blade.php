5de1a177fa2f23285d96b792ce7c669baeb0e0b3
istqbtesttrial <wajdib3@gmail.com>
2025-09-13 13:01:10 +0100

    Cleanup contact info

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -15,17 +15,9 @@
 
     <div class="container-xxl py-5">
         <div class="container">
-            <p>Voici ce que j’ai pu extraire de la page Facebook «&nbsp;EFC – Ecole de Formation des Cadres&nbsp;» ainsi que d’autres sources, pour que tu puisses intégrer ces infos dans ton site :</p>
-            <hr>
-            <h5 class="text-primary mb-3">Informations extraites</h5>
+            <h5 class="text-primary mb-3">Informations</h5>
             <div class="table-responsive">
                 <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Élément</th>
-                            <th>Détail</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         <tr>
                             <td><strong>Nom complet</strong></td>
@@ -33,61 +25,38 @@
                         </tr>
                         <tr>
                             <td><strong>Adresse principale</strong></td>
-                            <td>12 Rue de Grèce, Tunis, Tunisie (<a href="https://m.facebook.com/www.efc.tn/about/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
+                            <td>12 Rue de Grèce, Tunis, Tunisie</td>
                         </tr>
                         <tr>
                             <td><strong>Autre adresse</strong></td>
-                            <td>22 Rue El Jazira, Tunis (<a href="https://www.facebook.com/www.efc.tn/posts/d%C3%A9veloppez-votre-capacit%C3%A9-%C3%A0-converser-en-anglais-m%C3%A9dical-avec-confiance-en-prati/3955339901181828/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
+                            <td>22 Rue El Jazira, Tunis</td>
                         </tr>
                         <tr>
                             <td><strong>Téléphone(s)</strong></td>
                             <td>
-                                +216 29 769 773 (<a href="https://m.facebook.com/www.efc.tn/about/?utm_source=chatgpt.com" target="_blank">Facebook</a>)<br>
-                                +216 25 131 052 / +216 29 023 758 / +216 22 999 213 (<a href="https://www.facebook.com/www.efc.tn/videos/ecole-de-formation-des-cadres-ouverture-des-inscriptions-pour-la-session-f%C3%A9vrier/2530762847085219/?utm_source=chatgpt.com" target="_blank">Facebook</a>)
+                                +216 29 769 773<br>
+                                +216 25 131 052 / +216 29 023 758 / +216 22 999 213
                             </td>
                         </tr>
                         <tr>
-                            <td><strong>Nombre de “Likes / abonnés”</strong></td>
-                            <td>~ 123&nbsp;100 personnes aiment la page (<a href="https://www.facebook.com/www.efc.tn/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Nombre de personnes en discussion / actives (“talking about this”)</strong></td>
-                            <td>~ 260-309 (<a href="https://www.facebook.com/www.efc.tn/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Zone géographique</strong></td>
-                            <td>Tunis, Tunisie (<a href="https://m.facebook.com/www.efc.tn/about/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
-                        </tr>
-                        <tr>
                             <td><strong>Email</strong></td>
-                            <td><a href="mailto:efcformation01@gmail.com">efcformation01@gmail.com</a> (<a href="https://www.ecoles.com.tn/culture/ecole-de-formation-des-cadres-efc?utm_source=chatgpt.com" target="_blank">ecoles.com.tn</a>)</td>
+                            <td><a href="mailto:efcformation01@gmail.com">efcformation01@gmail.com</a></td>
                         </tr>
                         <tr>
                             <td><strong>Nature / activité</strong></td>
-                            <td>École de formation professionnelle privée, activités de formation, propose des trainings, etc. (<a href="https://www.ecoles.com.tn/culture/ecole-de-formation-des-cadres-efc?utm_source=chatgpt.com" target="_blank">ecoles.com.tn</a>)</td>
+                            <td>École de formation professionnelle privée, activités de formation, propose des trainings, etc.</td>
                         </tr>
                         <tr>
                             <td><strong>Création / reconnaissance</strong></td>
-                            <td>Créée en juin 2009, agréée / enregistrée auprès du ministère de la Formation professionnelle et de l’Emploi, numéro 09-986-11-1 (<a href="https://www.ecoles.com.tn/culture/ecole-de-formation-des-cadres-efc?utm_source=chatgpt.com" target="_blank">ecoles.com.tn</a>)</td>
+                            <td>Créée en juin 2009, agréée / enregistrée auprès du ministère de la Formation professionnelle et de l’Emploi, numéro 09-986-11-1</td>
                         </tr>
                         <tr>
                             <td><strong>Responsable principal</strong></td>
-                            <td>Jamel Ben Salah (<a href="https://www.ecoles.com.tn/culture/ecole-de-formation-des-cadres-efc?utm_source=chatgpt.com" target="_blank">ecoles.com.tn</a>)</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Mission / slogan visible</strong></td>
-                            <td>Dans un post : «&nbsp;Votre Succès c’est Notre Mission&nbsp;» (<a href="https://www.facebook.com/www.efc.tn/posts/d%C3%A9veloppez-votre-capacit%C3%A9-%C3%A0-converser-en-anglais-m%C3%A9dical-avec-confiance-en-prati/3955339901181828/?utm_source=chatgpt.com" target="_blank">Facebook</a>)</td>
+                            <td>Bargaoui Riadh</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
-            <h5 class="text-primary mb-3 mt-5">À vérifier / détails supplémentaires à confirmer</h5>
-            <ul>
-                <li>Le ou les emails officiels : le FB ne montre pas clairement un email pour contact principal, hormis celui trouvé sur un annuaire externe.</li>
-                <li>Horaires d’ouverture / jours de disponibilité téléphonique.</li>
-                <li>Si l’adresse «&nbsp;22 Rue el Jazira&nbsp;» est un siège ou une succursale ou si c’est juste un lieu d’activité.</li>
-                <li>Les spécialisations exactes des formations (domaines, durées) ne sont pas toutes visibles depuis FB.</li>
-            </ul>
         </div>
     </div>
 @endsection
